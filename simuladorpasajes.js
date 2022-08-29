1b773af9712bd7f3533e24dced534c77c65ec51e
Rocio Fabrykant <fabrykantr@gmail.com>
2022-08-29 19:11:49 -0300

    agregado de funcionalidad verificar capacidad

--- simuladorpasajes.js
+++ simuladorpasajes.js
@@ -1,6 +1,14 @@
 //SIMULADOR DE CALCULADOR COSTO PASAJE BUQUE. SE INGRESA UN ORIGEN Y UN DESTINO,
 //CANTIDAD DE PASAJEROS Y SI SE VIAJA O NO CON UN VEHICULO(MOTO, AUTO, CAMIONETA)
 //EN FUNCION DEL ORIGEN Y DESTINO SE CALCULA EL COSTO DE LOS PASAJES, INCLUYENDO IMPUESTOS
+let capacidad_total_pasajeros = 20;
+let capacidad_total_autos = 2;
+let capacidad_total_motos = 2;
+let capacidad_total_camionetas = 1;
+let totalpasajeros= 0;
+let acumuladorauto = 0;
+let acumuladormoto = 0;
+let acumuladorcamioneta = 0;
 class Viaje{
     constructor(origen,destino,cantidad_pasajeros,vehiculo){
         this.origen = origen;
@@ -59,8 +67,8 @@ class Viaje{
 }
 let arr_viajes = [];
 
-//SIMULO QUE INGRESAN DOS VIAJES
-for(i=0;i<2;i++){
+//SIMULO QUE INGRESAN TRES VIAJES
+for(i=0; i<4; i++) {
     let origen = prompt("Ingrese el origen: Buenos Aires/Colonia/Montevideo");
 while(origen != "Buenos Aires" && origen != "Colonia" && origen != "Montevideo"){
     alert("Ingrese correctamente el origen");
@@ -79,8 +87,17 @@ let cantidad_pasajeros = parseInt(prompt("Ingrese número de pasajeros"));
 
 while(cantidad_pasajeros <= 0 || isNaN(cantidad_pasajeros) || cantidad_pasajeros == null || cantidad_pasajeros == ""){
     alert("Solo puede ingresar numeros mayores a cero");
-    cantidad_pasajeros = parseInt(prompt("Ingrese número de pasajeros"));
+    cantidad_pasajeros = parseInt(prompt("Ingrese número de pasajeros"));   
 }
+totalpasajeros = totalpasajeros + cantidad_pasajeros;
+if(totalpasajeros> capacidad_total_pasajeros){
+        alert("No hay capacidad disponible");
+        totalpasajeros = totalpasajeros - cantidad_pasajeros;
+        cantidad_pasajeros = 0;
+        continue;
+    }
+
+
 let con_sin_vehiculo = prompt("Viaja con vehículo? indique SI o NO");;
 let vehiculo;
 while(con_sin_vehiculo != "Si" && con_sin_vehiculo != "SI" && con_sin_vehiculo != "si" && con_sin_vehiculo != "NO" && con_sin_vehiculo != "no" && con_sin_vehiculo != "No"){
@@ -91,6 +108,28 @@ while(con_sin_vehiculo != "Si" && con_sin_vehiculo != "SI" && con_sin_vehiculo !
 if(con_sin_vehiculo == "SI" || con_sin_vehiculo == "Si" || con_sin_vehiculo == "si"){
 do{    
     vehiculo = prompt("Indique el vehiculo moto/camioneta/auto");
+    if(vehiculo == "moto"){
+        acumuladormoto++;
+        if(acumuladormoto>capacidad_total_motos){
+            alert("No hay capacidad suficiente para motos");
+            vehiculo = "";
+            break;
+        }
+    }else if(vehiculo == "camioneta"){
+        acumuladorcamioneta++;
+        if(acumuladorcamioneta>capacidad_total_camionetas){
+            alert("No hay capacidad suficiente para camionetas");
+            vehiculo = "";
+            break;
+        }
+    }else if(vehiculo == "auto"){
+        acumuladorauto++;
+        if(acumuladorauto>capacidad_total_autos){
+            alert("No hay capacidad suficiente para autos");
+            vehiculo = "";
+            break;
+        }
+    }
 }while(vehiculo!= "moto" && vehiculo != "camioneta" && vehiculo != "auto")
 }
 let nuevo_viaje = new Viaje(origen,destino,cantidad_pasajeros,vehiculo);
@@ -110,6 +149,56 @@ for(let viaje of arr_viajes){
     console.log("Cantidad pasajeros:",viaje.cantidad_pasajeros);
     console.log("Vehiculo:",viaje.vehiculo);
 }
+
+//DETECTAR CAPACIDAD DISPONIBLE PASAJEROS Y VEHICULOS
+
+let acumulador_autos= 0;
+let acumulador_motos= 0;
+let acumulador_camionetas=0; 
+function acumulador_pasajeros(acu,arr_viajes){
+    acu = acu + arr_viajes.cantidad_pasajeros;
+    return acu
+}
+
+for(let viaje of arr_viajes){
+    if(viaje.vehiculo == "moto"){
+        acumulador_motos = acumulador_motos + 1;
+    }else if(viaje.vehiculo == "auto"){
+        acumulador_autos = acumulador_autos + 1;
+    }else if(viaje.vehiculo == "camioneta"){
+        acumulador_camionetas = acumulador_camionetas + 1;
+    }
+}
+if(acumulador_motos < capacidad_total_motos){
+    console.log("aun hay capacidad de motos quedan:",capacidad_total_motos - acumulador_motos, "lugares");
+}else if(acumulador_motos == capacidad_total_motos){
+    console.log("ya esta llena la capacidad de motos");
+}
+
+if(acumulador_autos < capacidad_total_autos){
+    console.log("aun hay capacidad de autos, quedan: ", capacidad_total_autos - acumulador_autos, "lugares");
+}else if(acumulador_autos == capacidad_total_autos){
+    console.log("ya esta completa la capacidad de pasajeros");
+}
+
+if(acumulador_camionetas < capacidad_total_camionetas){
+    console.log("aun hay capacidad de camionetas, quedan: ", capacidad_total_camionetas - acumulador_camionetas, "lugares");
+}else if(acumulador_camionetas == capacidad_total_camionetas){
+    console.log("la capacidad de camionetas esta llena");
+}
+
+let total_pasajeros = arr_viajes.reduce(acumulador_pasajeros,0);
+console.log("El total de pasajeros sería: ", total_pasajeros);
+if(total_pasajeros< capacidad_total_pasajeros){
+    console.log("El buque aun cuenta con capacidad de pasajeros quedan:", capacidad_total_pasajeros - total_pasajeros, "lugares");
+}else if(total_pasajeros == capacidad_total_pasajeros){
+    console.log("El buque ya está lleno");
+}
+
+
+
+
+
 
 
 
